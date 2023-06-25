94ecf01fe8e9c61f9d315e9be53891b30e9308cb
Thomas Sundell-Ekornrød <134838594+Samoht89@users.noreply.github.com>
2023-06-25 13:09:43 +0200

    Update index.pl

--- index.pl
+++ index.pl
@@ -10,7 +10,7 @@ print CGI::header();
 print <<HTML;
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Hovedside IKT </title>
+<title>Hovedside </title>
 </head>
 <body>
 <center>
